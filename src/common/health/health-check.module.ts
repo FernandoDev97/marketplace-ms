875b5b9ea8de7724363b452309b2522a666d59c0
Fernando Souza <l.fernando.dev@gmail.com>
2026-04-12 13:35:53 -0300

    feat: create helth controller and fix imports

--- src/common/health/health-check.module.ts
+++ src/common/health/health-check.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
+import { CircuitBreakerModule } from '../circuit-breaker/circuit-breaker.module';
 import { HealthCheckService } from './health-check.service';
 
 @Module({
+  imports: [HttpModule, CircuitBreakerModule],
   providers: [HealthCheckService],
   exports: [HealthCheckService],
 })
---
+++ src/health/health.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { HealthController } from './health.controller';
+import { HealthService } from './health.service';
+import { HealthCheckModule } from 'src/common/health/health-check.module';
+
+@Module({
+  imports: [HealthCheckModule],
+  controllers: [HealthController],
+  providers: [HealthService],
+  exports: [HealthService],
+})
+export class HealthModule {}
